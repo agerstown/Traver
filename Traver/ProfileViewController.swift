c316ad3d4c2dfbdffbe7e851f02cfe80b9469460
agerstown <nata.nikitina95@mail.ru>
2016-11-22 23:39:23 +0300

    profile screen

--- Traver/ProfileViewController.swift
+++ Traver/ProfileViewController.swift
@@ -21,7 +21,10 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // setting up the map
+        tableViewCountries.dataSource = self
+        tableViewCountries.delegate = self
+        
+        // setting up the map and it's size
         let mapImage = SVGKImage(named: "WorldMap.svg")!
         let width = viewMap.frame.size.width
         let scale = width / mapImage.size.width
@@ -31,7 +34,71 @@ class ProfileViewController: UIViewController {
         if let imageView = SVGLayeredImageView {
             viewMap.addSubview(imageView)
         }
+        
+//        let mainLayer = mapImage.caLayerTree
+//        let subLayers = mainLayer?.sublayers
+//        if let subLayers = subLayers {
+//            for layer in subLayers {
+//                print(layer)
+//                let subSubs = layer.sublayers
+//                if let subSubs = subSubs {
+//                    for subSub in subSubs {
+//                        print(subSub.name)
+//                    }
+//                }
+//            }
+//        }
+
     }
     
 }
 
+extension ProfileViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return sections[section]
+//    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 176
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableViewCountries.dequeueReusableCell(withIdentifier: "CountryItemCell") as! CountryItemCell
+        cell.labelCountryName.text = Countries.countries[indexPath.row]
+        
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if (editingStyle == UITableViewCellEditingStyle.delete) {
+//            var mapToDelete: Map?
+//            if indexPath.section == 0 {
+//                mapToDelete = permanentMaps[indexPath.row]
+//            }
+//            else {
+//                mapToDelete = temporaryMaps[indexPath.row]
+//            }
+//            User.currentUser?.maps.removeObject(mapToDelete!)
+//            mapsList.removeObject(mapToDelete!)
+//            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+//        }
+//    }
+
+}
+
+extension ProfileViewController: UITableViewDelegate {
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//    }
+    
+}
---
+++ Traver/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Traver
+//
+//  Created by Natalia Nikitina on 11/22/16.
+//  Copyright © 2016 Natalia Nikitina. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
